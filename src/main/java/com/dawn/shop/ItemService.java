93114ccca58f11a82274a3824ddb28d094cc769f
hiJin97 <hnh092600@gmail.com>
2024-11-23 18:33:11 +0900

    Item Not Found Exception 생성하여 예외처리 관리

---
+++ src/main/java/com/dawn/shop/ItemService.java
@@ -0,0 +1,30 @@
+package com.dawn.shop;
+
+import com.dawn.shop.common.exception.ItemNotFoundException;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class ItemService {
+
+
+    private final ItemRepository itemRepository;
+
+    public void addItem(Item item) {
+        itemRepository.save(item);
+    }
+
+    public List<Item> findAllItems() {
+        return itemRepository.findAll();
+    }
+
+    public Item findByIdItems(Long id) {
+        return itemRepository.findById(id).orElseThrow(() -> new ItemNotFoundException("Item Not found"));
+    }
+
+}
